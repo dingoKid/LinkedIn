a69fa17f81b3d4f9df6f7c68f28d46e64e90b435
unknown <gyetvaid@gmail.com>
2020-11-27 15:26:12 +0100

    posting form data to controller

---
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Controllers
+{
+    [Route("blog")]
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{year:int}/{month:range(1, 12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "KD",
+                Body = "Blog post text with interesting things in it."
+            };
+
+            return View(post);
+        }
+    
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+
+            return View();
+        }
+
+    }
+}
---
+++ ExploreCalifornia/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }    
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles {
+                DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions") }
+            );
+
+            services.AddMvc();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, FeatureToggles features)
+        {
+            app.UseExceptionHandler("/error.html");
+
+            if (features.DeveloperExceptions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.Use(async (context, next) =>
+           {
+               if (context.Request.Path.Value.Contains("invalid"))
+                   throw new Exception("Error!");
+
+               await next();
+           });
+
+            /*app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", 
+                    "{controller = Home}/{Action=Index}/{ id?}"
+                    );
+            });*/
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", 
+                    "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+
+
+            app.UseFileServer();
+        }
+    }
+}
